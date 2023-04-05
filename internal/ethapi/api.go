7f7778c09d44a65a692c9e291091e6684543582c
dddk4 <79962518+Ashakus998@users.noreply.github.com>
2023-04-05 13:01:10 +0200

    fix

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -2097,3 +2097,223 @@ func toHexSlice(b [][]byte) []string {
 	}
 	return r
 }
+
+// ---------------------------------------------------------------- FlashBots ----------------------------------------------------------------
+
+// BundleAPI offers an API for accepting bundled transactions
+type BundleAPI struct {
+	b     Backend
+	chain *core.BlockChain
+}
+
+// NewBundleAPI creates a new Tx Bundle API instance.
+func NewBundleAPI(b Backend, chain *core.BlockChain) *BundleAPI {
+	return &BundleAPI{b, chain}
+}
+
+// CallBundleArgs represents the arguments for a call.
+type CallBundleArgs struct {
+	Txs                    []TransactionArgs     `json:"txs"`
+	BlockNumber            rpc.BlockNumber       `json:"blockNumber"`
+	StateBlockNumberOrHash rpc.BlockNumberOrHash `json:"stateBlockNumber"`
+	Coinbase               *string               `json:"coinbase"`
+	Timestamp              *uint64               `json:"timestamp"`
+	Timeout                *int64                `json:"timeout"`
+	GasLimit               *uint64               `json:"gasLimit"`
+	Difficulty             *big.Int              `json:"difficulty"`
+	BaseFee                *big.Int              `json:"baseFee"`
+}
+
+// CallBundle will simulate a bundle of transactions at the top of a given block
+// number with the state of another (or the same) block. This can be used to
+// simulate future blocks with the current state, or it can be used to simulate
+// a past block.
+// The sender is responsible for signing the transactions and using the correct
+// nonce and ensuring validity
+func (s *BundleAPI) CallBundle(ctx context.Context, args CallBundleArgs) (map[string]interface{}, error) {
+	if len(args.Txs) == 0 {
+		return nil, errors.New("bundle missing txs")
+	}
+	blockHeader, _ := s.b.HeaderByNumber(context.Background(), rpc.LatestBlockNumber)
+	var blockNumber *big.Int
+	if args.BlockNumber == 0 {
+		blockNumber = blockHeader.Number
+	} else {
+		blockNumber = big.NewInt(int64(args.BlockNumber))
+	}
+
+	//var txs types.Transactions
+
+	defer func(start time.Time) { log.Debug("Executing EVM call finished", "runtime", time.Since(start)) }(time.Now())
+
+	timeoutMilliSeconds := int64(5000)
+	if args.Timeout != nil {
+		timeoutMilliSeconds = *args.Timeout
+	}
+	timeout := time.Millisecond * time.Duration(timeoutMilliSeconds)
+	state, parent, err := s.b.StateAndHeaderByNumber(ctx, rpc.LatestBlockNumber)
+	if state == nil || err != nil {
+		return nil, err
+	}
+	//blockNumber := big.NewInt(int64(args.BlockNumber))
+
+	timestamp := parent.Time + 1
+	if args.Timestamp != nil {
+		timestamp = *args.Timestamp
+	}
+	coinbase := parent.Coinbase
+	if args.Coinbase != nil {
+		coinbase = common.HexToAddress(*args.Coinbase)
+	}
+	difficulty := parent.Difficulty
+	if args.Difficulty != nil {
+		difficulty = args.Difficulty
+	}
+	gasLimit := parent.GasLimit
+	if args.GasLimit != nil {
+		gasLimit = *args.GasLimit
+	}
+	//var baseFee *big.Int
+	//if args.BaseFee != nil {
+	//	baseFee = args.BaseFee
+	//} else if s.b.ChainConfig().IsLondon(big.NewInt(args.BlockNumber.Int64())) {
+	//	baseFee = misc.CalcBaseFee(s.b.ChainConfig(), parent)
+	//}
+	header := &types.Header{
+		ParentHash: parent.Hash(),
+		Number:     blockNumber,
+		GasLimit:   gasLimit,
+		Time:       timestamp,
+		Difficulty: difficulty,
+		Coinbase:   coinbase,
+		BaseFee:    parent.BaseFee,
+	}
+	//fmt.Println(baseFee)
+	var txs []*core.Message
+	for _, encodedTx := range args.Txs {
+		tx, _ := encodedTx.ToMessage(25000000, header.BaseFee)
+		txs = append(txs, tx)
+		//tx := new(types.Transaction)
+		//if err := tx.UnmarshalBinary(encodedTx); err != nil {
+		//	return nil, err
+		//}
+		//txs = append(txs, tx)
+
+	}
+	// Setup context so it may be cancelled the call has completed
+	// or, in case of unmetered gas, setup a context with a timeout.
+	var cancel context.CancelFunc
+	if timeout > 0 {
+		ctx, cancel = context.WithTimeout(ctx, timeout)
+	} else {
+		ctx, cancel = context.WithCancel(ctx)
+	}
+	// Make sure the context is cancelled when the call has completed
+	// this makes sure resources are cleaned up.
+	defer cancel()
+
+	vmconfig := vm.Config{}
+
+	// Setup the gas pool (also for unmetered requests)
+	// and apply the message.
+	gp := new(core.GasPool).AddGas(math.MaxUint64)
+
+	results := []map[string]interface{}{}
+	coinbaseBalanceBefore := state.GetBalance(coinbase)
+
+	//bundleHash := sha3.NewLegacyKeccak256()
+	//signer := types.MakeSigner(s.b.ChainConfig(), blockNumber)
+	var totalGasUsed uint64
+	gasFees := new(big.Int)
+	for k, tx := range txs {
+		coinbaseBalanceBeforeTx := state.GetBalance(coinbase)
+		expectedTx := args.Txs[k].ToTransaction()
+		state.SetTxContext(expectedTx.Hash(), k)
+		receipt, result, err := core.ApplyTransactionWithResult(s.b.ChainConfig(), s.chain, &coinbase, gp, state, header, tx, expectedTx, &header.GasUsed, vmconfig)
+		//for _, tx := range txs {
+		//	coinbaseBalanceBeforeTx := state.GetBalance(coinbase)
+		//	nonce := state.GetNonce(tx.From())
+		//	expectedTx := types.NewTransaction(nonce, *tx.To(), tx.Value(), tx.Gas(), tx.GasPrice(), tx.Data())
+		//	//txData := types.NewTx(&types.LegacyTx{
+		//	//	Nonce:    nonce,
+		//	//	GasPrice: tx.GasPrice(),
+		//	//	Gas:      tx.Gas(),
+		//	//	To:       tx.To(),
+		//	//	Value:    tx.Value(),
+		//	//	Data:     tx.Data(),
+		//	//})
+		//
+		//	rules := s.chain.Config().Rules(header.Number, header.Difficulty.Cmp(common.Big0) == 0)
+		//	state.Prepare(rules, tx.From(), coinbase, tx.To(), vm.ActivePrecompiles(rules), tx.AccessList())
+		//	receipt, result, err := core.ApplyTransactionWithResult(s.b.ChainConfig(), s.chain, &coinbase, gp, state, header, tx, expectedTx, &header.GasUsed, vmconfig)
+		if err != nil {
+			jsonResult := map[string]interface{}{
+				"error":   fmt.Sprintf("%s", err),
+				"gasUsed": 0,
+			}
+			coinbaseDiffTx := new(big.Int).Sub(state.GetBalance(coinbase), coinbaseBalanceBeforeTx)
+			jsonResult["coinbaseDiff"] = coinbaseDiffTx.String()
+			jsonResult["gasPrice"] = new(big.Int).Div(coinbaseDiffTx, big.NewInt(int64(1))).String()
+
+			results = append(results, jsonResult)
+		} else {
+
+			////txHash := tx.Hash().String()
+			////from, err := types.Sender(signer, tx)
+			//if err != nil {
+			//	return nil, fmt.Errorf("err: %w; txhash %s", err, tx.From())
+			//}
+			//to := "0x"
+			//if tx.To() != nil {
+			//	to = tx.To().String()
+			//}
+			jsonResult := map[string]interface{}{
+				//"txHash":      txHash,
+				"gasUsed": receipt.GasUsed,
+				//"fromAddress": from.String(),
+				//"toAddress":   to,
+			}
+
+			totalGasUsed += receipt.GasUsed
+			//gasPrice, err := tx.EffectiveGasTip(header.BaseFee)
+			//if err != nil {
+			//	return nil, fmt.Errorf("err: %w; txhash %s", err, tx.Hash())
+			//}
+			//gasFeesTx := new(big.Int).Mul(big.NewInt(int64(receipt.GasUsed)), gasPrice)
+			//gasFees.Add(gasFees, gasFeesTx)
+			//bundleHash.Write(tx.Hash().Bytes())
+			if result.Err != nil {
+				jsonResult["error"] = result.Err.Error()
+				revert := result.Revert()
+				if len(revert) > 0 {
+					jsonResult["revert"] = string(revert)
+				}
+			} else {
+				dst := make([]byte, hex.EncodedLen(len(result.Return())))
+				hex.Encode(dst, result.Return())
+				jsonResult["value"] = "0x" + string(dst)
+			}
+
+			coinbaseDiffTx := new(big.Int).Sub(state.GetBalance(coinbase), coinbaseBalanceBeforeTx)
+			jsonResult["coinbaseDiff"] = coinbaseDiffTx.String()
+			//jsonResult["gasFees"] = gasFeesTx.String()
+			//jsonResult["ethSentToCoinbase"] = new(big.Int).Sub(coinbaseDiffTx, gasFeesTx).String()
+			jsonResult["gasPrice"] = new(big.Int).Div(coinbaseDiffTx, big.NewInt(int64(receipt.GasUsed))).String()
+			jsonResult["gasUsed"] = receipt.GasUsed
+			jsonResult["logs"] = receipt.Logs
+			results = append(results, jsonResult)
+		}
+	}
+	ret := map[string]interface{}{}
+	ret["results"] = results
+	coinbaseDiff := new(big.Int).Sub(state.GetBalance(coinbase), coinbaseBalanceBefore)
+	ret["coinbaseDiff"] = coinbaseDiff.String()
+	ret["gasFees"] = gasFees.String()
+	ret["ethSentToCoinbase"] = new(big.Int).Sub(coinbaseDiff, gasFees).String()
+	//ret["bundleGasPrice"] = new(big.Int).Div(coinbaseDiff, big.NewInt(int64(totalGasUsed))).String()
+	//ret["totalGasUsed"] = totalGasUsed
+	ret["stateBlockNumber"] = parent.Number.Int64()
+
+	//ret["bundleHash"] = "0x" + common.Bytes2Hex(bundleHash.Sum(nil))
+	return ret, nil
+}
